19dbb3ee35e5089bab428e9a9353742f2b5d5aee
George Shaikovski <g.shaikov@gmail.com>
2020-10-28 23:05:07 +0000

    recover: find jpegs

--- recover/recover.c
+++ recover/recover.c
@@ -28,34 +28,60 @@ int main(int argc, char *argv[])
     return result;
 }
 
+int jpeg_start(Byte *buf)
+{
+    return buf[0] == 0xff && buf[1] == 0xd8 && buf[2] == 0xff && (buf[3] >> 4) == 0xe;
+}
+
+int fclose_if_open(FILE *f)
+{
+    if (f != NULL)
+    {
+        return fclose(f);
+    }
+    return 0;
+}
+
+int openned(FILE *f)
+{
+    return f != NULL;
+}
+
 int recover_images(FILE *infile, char *infile_name, char *outfile_dir)
 {
-    FILE *jpeg;
+    FILE *jpeg = NULL;
     Byte buf[FAT_BLOCK_SIZE_BYTES];
     char jpeg_path[100];
+    int count = 0;
 
-    sprintf(jpeg_path, "%s/%3.3i.jpg", outfile_dir, 0);
-    jpeg = fopen(jpeg_path, "w");
-    if (jpeg == NULL)
-    {
-        printf("failed to open file: %s\n", jpeg_path);
-        return 1;
-    }
     while (fread(buf, sizeof(Byte), FAT_BLOCK_SIZE_BYTES, infile) == sizeof(Byte) * FAT_BLOCK_SIZE_BYTES)
     {
-        if (fwrite(buf, sizeof(Byte), FAT_BLOCK_SIZE_BYTES, jpeg) != sizeof(Byte) * FAT_BLOCK_SIZE_BYTES)
+        if (jpeg_start(buf))
+        {
+            fclose_if_open(jpeg);
+            sprintf(jpeg_path, "%s/%3.3i.jpg", outfile_dir, count);
+            count++;
+            jpeg = fopen(jpeg_path, "w");
+            if (jpeg == NULL)
+            {
+                printf("failed to open file: %s\n", jpeg_path);
+                return 1;
+            }
+        }
+        if (openned(jpeg) && fwrite(buf, sizeof(Byte), FAT_BLOCK_SIZE_BYTES, jpeg) != sizeof(Byte) * FAT_BLOCK_SIZE_BYTES)
         {
             printf("failed to write a block of %i bytes into file %s\n", FAT_BLOCK_SIZE_BYTES, jpeg_path);
-            fclose(jpeg);
+            fclose_if_open(jpeg);
             return 1;
         }
     }
-    fclose(jpeg);
 
-    if (feof(infile))
+    fclose_if_open(jpeg);
+
+    if (!feof(infile))
     {
-        return 0;
+        printf("failed to read a block of %i bytes from file %s\n", FAT_BLOCK_SIZE_BYTES, infile_name);
+        return 1;
     }
-    printf("failed to read a block of %i bytes from file %s\n", FAT_BLOCK_SIZE_BYTES, infile_name);
-    return 1;
+    return 0;
 }
